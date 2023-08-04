27b04cc411d9844e9a764f496246d1f9cc31beb6
rainy-xie <1972575545@qq.com>
2023-08-04 18:44:49 +0800

    mobile test

--- hw/hw04/hw04/hw04.py
+++ hw/hw04/hw04/hw04.py
@@ -65,12 +65,13 @@ def planet(mass):
     """Construct a planet of some mass."""
     assert mass > 0
     "*** YOUR CODE HERE ***"
-
+    return ['planet',mass]
 
 def mass(w):
     """Select the mass of a planet."""
     assert is_planet(w), 'must call mass on a planet'
     "*** YOUR CODE HERE ***"
+    return w[1]
 
 
 def is_planet(w):
