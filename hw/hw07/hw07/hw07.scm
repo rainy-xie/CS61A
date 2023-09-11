a83464924d035998365489eef5abe19b3d6f8ddc
WorkXie <1972575545@qq.com>
2023-09-11 18:50:56 +0800

    hw07

--- hw/hw07/hw07/hw07.scm
+++ hw/hw07/hw07/hw07.scm
@@ -1,10 +1,17 @@
 (define (cddr s) (cdr (cdr s)))
 
-(define (cadr s) 'YOUR-CODE-HERE)
+(define (cadr s) (car (cdr s)))
 
-(define (caddr s) 'YOUR-CODE-HERE)
+(define (caddr s) (car (cdr (cdr s))))
 
-(define (ascending? asc-lst) 'YOUR-CODE-HERE)
+(define (ascending? asc-lst) 
+    (cond 
+        ((or (null? asc-lst) (null? (cdr asc-lst))) #t) ; 如果列表为空或只有一个元素，认为是非降序
+        ((<= (car asc-lst) (cadr asc-lst)) (ascending? (cdr asc-lst))); 当前元素小于等于下一个元素，递归检查其余部分
+        (else #f); 当前元素大于下一个元素，不是非降序
+        )
+    )
+
 
 (define (square n) (* n n))
 
